b801217c819914729af313de4ee88e3df3ec7021
mike cullerton <michaelc@cullerton.com>
2019-02-01 10:32:39 -0500

    Added default view for Forum

--- kotti_forum/views/view.py
+++ kotti_forum/views/view.py
@@ -1,6 +1,22 @@
-# from pyramid.view import view_config
-# from pyramid.view import view_defaults
+from pyramid.view import view_config
+from pyramid.view import view_defaults
 
 # from kotti_forum import _
-# from kotti_forum.fanstatic import css_and_js
-# from kotti_forum.views import BaseView
+from kotti_forum.fanstatic import css_and_js
+from kotti_forum.views import BaseView
+
+from kotti_forum.resources import Forum
+
+
+@view_defaults(context=Forum)
+class ForumViews(BaseView):
+    """ Views for :class:`kotti_forum.resources.Forum` """
+
+    @view_config(name='view', permission='view',
+                 renderer='kotti_forum:templates/forum.pt')
+    def forum_view(self):
+        css_and_js.need()
+        ideas = self.context.children
+        return {
+            'ideas': ideas,
+        }
